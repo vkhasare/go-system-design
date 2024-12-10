d7345812add8f20033eddc80876bff8b31cfa3f8
Vrushali Khasare <vrushali.khasare@gmail.com>
2024-12-10 11:04:34 -0500

    Adding URL expiration timestamp

--- 01-url-shortening/db.sql
+++ 01-url-shortening/db.sql
@@ -4,6 +4,7 @@ CREATE TABLE short_urls (
     short_url TEXT NOT NULL,
     user_id VARCHAR(255),
     qr_code TEXT,
+    expires_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     created_by VARCHAR(255) NOT NULL,
     created_date TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (now() AT TIME ZONE 'UTC'),
     last_modified_by VARCHAR(255),
