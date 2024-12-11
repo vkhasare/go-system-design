ee4f5e80839fa41f6bd90710ae02db47ddee4f51
Vrushali Khasare <vrushali.khasare@gmail.com>
2024-12-11 10:51:16 -0500

    Adding first rest endpoint

--- 01-url-shortening/cmd/main.go
+++ 01-url-shortening/cmd/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("TH!")
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"response": "pong"})
+	})
+
+	r.Run(":8053")
 }
